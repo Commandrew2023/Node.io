27f8a6d9060b6a29e8992cab1c38939cb38b0164
Commandrew2023 <120763631+Commandrew2023@users.noreply.github.com>
2026-08-23 11:23:04 -0500

    Add Popup class for message display functionality
    Implemented a Popup class for displaying messages with a duration and automatic removal.

---
+++ components/popup.js
@@ -0,0 +1,66 @@
+(function () {
+  class Popup {
+    constructor (title, body) {
+      this.DOM_Element = null;
+
+      this.message = { title, body };
+      
+      this.duration = Infinity;
+      this.#currentStep = 0;
+      this.#updateLoop = setInterval((that) => {
+        that.#currentStep++;
+        if (that.#currentStep >= that.duration) {
+          that.#setInactive();
+          setTimeout((that) => {
+            that.remove();
+          }, 2000, that);
+        }
+      }, 1, this);
+    }
+    #setActive () {
+      if (this.DOM_Element !== null) {
+        this.DOM_Element.setAttribute('popup-active', true);
+      }
+    }
+    #setInactive () {
+      if (this.DOM_Element !== null) {
+        this.DOM_Element.setAttribute('popup-active', false);
+      }
+    }
+    setDuration (duration) {
+      this.duration = duration;
+    }
+    deploy () {
+
+      /* Build popup node */
+      const popup = HTML_Build({
+        type : 'div',
+        classes : ['popup'],
+        children : [
+          {
+            type : 'div',
+            html : this.message.title,
+          },
+          {
+            type : 'div',
+            html : this.message.body
+          }
+        ]
+      });
+
+      /* Bind popup node to object */
+      this.DOM_Element = popup;
+      this.#setActive();
+
+      /* Append node to body */
+      document.querySelector('body').appendChild(popup);
+
+      /* Redefine Popups flat */
+      module.Flats.Popups = [this];
+    }
+    remove () {
+      this.DOM_Element.remove();
+      module.Flats.Popups = [];
+    }
+  }
+})();
