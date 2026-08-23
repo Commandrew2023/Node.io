4e200b104b19b7b941eb3a4ee67108a00e2f4639
Commandrew2023 <120763631+Commandrew2023@users.noreply.github.com>
2026-08-23 00:58:02 -0500

    Update preload.js

--- main/preload.js
+++ main/preload.js
@@ -1,3 +1,24 @@
 (function (module) {
-  
+    module.root = `https://cdn.jsdelivr.net/gh/Commandrew2023/Node.io@main/`;
+    module.requestJS = (path) => {
+        return new Promise(
+            async (resolve, reject) => {
+                const response = await fetch(`module.root${path}`);
+                if (!response.ok) {
+                    reject(response);
+                    throw new Error(`Error loading js file: ${path}`);
+                } else {
+                    resolve(response);
+                }
+            }
+        ).then((res) => {
+            console.log(res);
+        });
+    };
+    module.loadRequests = () => {
+        module.requests.forEach(request => module.requestJS(request));
+    };
+    module.requests = [
+        "main/index.js"
+    ];
 })(this);
