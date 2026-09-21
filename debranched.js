c4cce41ec30d3c0940859a01e8fd3ed01d74fa29
Commandrew2023 <120763631+Commandrew2023@users.noreply.github.com>
2026-09-20 23:24:35 -0500

    Fix event handling for ribbon buttons

--- debranched.js
+++ debranched.js
@@ -8347,8 +8347,8 @@
             const buttons = document.querySelectorAll('.rbutton');
             [...buttons].forEach(btn => {
                 let button_name = btn.getAttribute('name');
-                console.log(btn.getAttribute('hasevent'), Boolean(btn.getAttribute('hasevent')));
-                if (Boolean(btn.getAttribute('hasevent'))) {
+                //console.log(btn.getAttribute('hasevent'), Boolean(btn.getAttribute('hasevent')));
+                if (btn.getAttribute('hasevent') === 'true') {
                     console.log('Added to ' + button_name);
                     btn.addEventListener('click', e => {
                         console.log('Testing Button Point');
@@ -8356,7 +8356,7 @@
                         this.ribbonButtonHandlers[name].click(e.currentTarget);
                     });
 
-                    btn.setAttribute('hasevent', false);
+                    btn.setAttribute('hasevent', true);
                 }
                 this.ribbonButtonHandlers[button_name].update(btn);
             });
