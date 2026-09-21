f0b1f7ad9a6019bbb153a994847337192717de01
Commandrew2023 <120763631+Commandrew2023@users.noreply.github.com>
2026-09-20 23:25:44 -0500

    Update debranched.js

--- debranched.js
+++ debranched.js
@@ -8348,8 +8348,8 @@
             [...buttons].forEach(btn => {
                 let button_name = btn.getAttribute('name');
                 //console.log(btn.getAttribute('hasevent'), Boolean(btn.getAttribute('hasevent')));
-                if (btn.getAttribute('hasevent') === 'true') {
-                    console.log('Added to ' + button_name);
+                if (btn.getAttribute('hasevent') === null) {
+                    //console.log('Added to ' + button_name);
                     btn.addEventListener('click', e => {
                         console.log('Testing Button Point');
                         let name = e.currentTarget.getAttribute('name');
