d2a41dbb6e7482d8439c3f8eccec4804e784cb5c
Commandrew2023 <120763631+Commandrew2023@users.noreply.github.com>
2026-09-26 15:10:33 -0500

    Clean up debranched.js by removing comments
    Removed commented-out code for button event handling.

--- debranched.js
+++ debranched.js
@@ -8349,14 +8349,15 @@
                 let button_name = btn.getAttribute('name'),
                     hasEvent = btn.getAttribute('hasevent');
                 console.log(hasEvent, Boolean(hasEvent));
-                /*if (hasEvent === null) {
-                    console.log('Added to ' + button_name);
+                if (hasEvent === null) {
+                    console.log('Is Null');
+                    /*console.log('Added to ' + button_name);
                     btn.addEventListener('click', e => {
                         alert("testing");
                         //console.log('Testing Button Point');
                         let name = e.currentTarget.getAttribute('name');
                         this.ribbonButtonHandlers[name].click(e.currentTarget);
-                    });
+                    });*/
 
                     btn.setAttribute('hasevent', true);
                 }
